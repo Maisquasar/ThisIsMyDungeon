6d9b06e62da8a3f44f1b770359b306408202552b
Umut OSMANOGLU <u.osmanoglu@student.isartdigital.com>
2022-11-23 15:22:56 +0100

    Spawner Update

---
+++ ThisIsMyDungeon/Source/ThisIsMyDungeon/DungeonGameMode.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DungeonGameMode.h"
+#include "Runtime/Engine/Public/TimerManager.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void ADungeonGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	TSubclassOf<ASpawner> ClassToFind;
+	ClassToFind = ASpawner::StaticClass();
+	TArray<AActor*> TempSpawner;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ClassToFind, TempSpawner);
+
+	for (int i = 0; i < TempSpawner.Num() - 1; i++)
+	{
+		Spawners.Add(Cast<ASpawner>(TempSpawner[i]));
+	}
+}
+
+void ADungeonGameMode::StartWave()
+{
+	//FTimerHandle Handle2;
+	//GetWorld()->GetTimerManager().SetTimer(Handle2, this, , 100.f);
+
+	for (int i = 0; i <= Spawners.Num() - 1; i++)
+	{
+		for (int j = 0; j <= Spawners[i]->ArrayOfWaves.Num() - 1; j++)
+		{
+			if(Spawners[i]->ArrayOfWaves[j] == currentWave)
+			{
+				Spawners[i]->SpawnEnemy();
+			}
+		}
+	}
+}
+
+
+
---
+++ ThisIsMyDungeon/Source/ThisIsMyDungeon/DungeonGameMode.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Spawner/Spawner.h"
+#include "DungeonGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THISISMYDUNGEON_API ADungeonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere) 
+		int currentWave = 0;
+
+	UPROPERTY(EditAnywhere) 
+		int waveState = 0;
+
+	UPROPERTY(EditAnywhere)
+		int treasureLife = 25;
+
+	UPROPERTY(EditAnywhere) 
+		TArray<ASpawner*> Spawners;
+
+public:
+
+	void StartWave();
+	
+	
+	
+};
