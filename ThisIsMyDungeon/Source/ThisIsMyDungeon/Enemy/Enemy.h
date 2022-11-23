b9cd4fa6e14230fbd5e83f4d0d0717d54dc3e522
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2022-11-23 13:57:52 +0100

    Beginning of enemy

---
+++ ThisIsMyDungeon/Source/ThisIsMyDungeon/Enemy/Enemy.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class THISISMYDUNGEON_API AEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly, Category = "AI")
+		class UBehaviorTree* TreeAsset;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
